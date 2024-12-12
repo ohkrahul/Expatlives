ffc6cc8037598c9275636f8448acddeaeda53f79
Ohkrahull <rahul@beeyond.agency>
2024-12-12 21:19:56 +0530

    added somenew

---
+++ google sheet3.js
@@ -0,0 +1,11 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwSa1OGZaHFktNPelAnXWo_C9eUToCkNz_Rf8afJIDZzxr5EMEDhxmtbxcRVJrZgVQEbg/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit' , e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'post', body: new FormData(form)})
+    .then(response => alert("Thank you! Your form is submitted successfully."))
+    .then(() => { window.location.reload(); })
+    .catch(error => console.error('Error!', error.message))
+})
